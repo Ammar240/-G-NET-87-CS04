4e41e265fe5856e25fbab318fc7e36dc3f100d36
Ammar <eammar240@gmail.com>
2026-07-29 01:36:12 +0300

    solve question 3

--- Methods_Assingnment/Program.cs
+++ Methods_Assingnment/Program.cs
@@ -14,6 +14,15 @@ internal class Program
         PrintBookTitle("Clean Code");
         PrintLine();
         #endregion
+
+        #region question 3
+        int pages = 400;
+        int total = AddBounusPages(pages);
+        Console.WriteLine(pages); // I expect to see 400, because this method does not change the value of the variable pages,
+                                  // it only returns a new value (call by value)
+        Console.WriteLine(total);
+        PrintLine();
+        #endregion
     }
     #region question 1
     static void PrintWelcomeMessage()
@@ -29,6 +38,12 @@ internal class Program
     }
     #endregion
 
+    #region question 3
+    static int AddBounusPages(int pages)
+    {
+        return pages + 50;
+    }
+    #endregion
 
     //this method for me to format output in console
     static void PrintLine()
