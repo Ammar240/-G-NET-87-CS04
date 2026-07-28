108900113ffd9360f447a4173d3cfa39ae1ddbf7
Ammar <eammar240@gmail.com>
2026-07-29 01:21:04 +0300

    solve question 1

--- Methods_Assingnment/Program.cs
+++ Methods_Assingnment/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Methods_Assingnment
+﻿namespace Methods_Assingnment;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        #region question 1
+        PrintWelcomeMessage();
+
+        Console.WriteLine("============================");
+        #endregion
     }
+    #region question 1
+    static void PrintWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Library!");
+    } 
+    #endregion
 }
