2c6f0455fcb3842912a3468b840195ccf3edccab
Ammar <eammar240@gmail.com>
2026-07-29 01:28:04 +0300

    solve question 2

--- Methods_Assingnment/Program.cs
+++ Methods_Assingnment/Program.cs
@@ -9,11 +9,30 @@ internal class Program
 
         Console.WriteLine("============================");
         #endregion
+
+        #region question 2
+        PrintBookTitle("Clean Code");
+        PrintLine();
+        #endregion
     }
     #region question 1
     static void PrintWelcomeMessage()
     {
         Console.WriteLine("Welcome to the Library!");
-    } 
+    }
     #endregion
+
+    #region question 2
+    static void PrintBookTitle(string title)
+    {
+        Console.WriteLine($"Book title: {title}");
+    }
+    #endregion
+
+
+    //this method for me to format output in console
+    static void PrintLine()
+    {
+        Console.WriteLine("============================");
+    }
 }
