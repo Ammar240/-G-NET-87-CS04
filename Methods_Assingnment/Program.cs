0276dc4f5c462b87ec84cce30dd2bac672c566eb
Ammar <eammar240@gmail.com>
2026-07-29 01:02:49 +0300

    initial commit

---
+++ Methods_Assingnment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Methods_Assingnment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
